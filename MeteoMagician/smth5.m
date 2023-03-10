2a82f95de89f523372b6a3c7a071ed45369087d2
DYOfRomanum <103297600+DYOfRomanum@users.noreply.github.com>
2023-03-10 21:12:58 +0800

    Delete MeteoMagician directory

--- MeteoMagician/smth5.m
+++
@@ -1,4 +0,0 @@
-function F1=smth5(F,s)
-%% ÎåµãÆ½»¬
-F1=F;
-F1(2:end-1,2:end-1)=F(2:end-1,2:end-1)+s/4*(F(3:end,2:end-1)+F(1:end-2,2:end-1)+F(2:end-1,3:end)+F(2:end-1,1:end-2)-4*F(2:end-1,2:end-1));
--- MeteoMagician/static_stability.m
+++
@@ -1,7 +0,0 @@
-function [ss]=static_stability(T,pressure)
-%% 计算静力稳定度，输入温度T(isobaric,lat,lon)、一维气压
-theta=potential_temperature(T,pressure);%位温
-LnTheta=log(theta);
-rho=density(T,pressure);%空气密度
-plnt_pp=gradient_vert(LnTheta,pressure);
-ss=-1/rho.*plnt_pp;
